7dcb829249bc9c89ffc76132b43ae637680c457e
Mark Lester <93200590+MarkLesterB@users.noreply.github.com>
2023-08-04 10:48:26 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
